3412aa6446bf25d2a56350d23988d56cdfbb6d0e
mohamed seddik <mohsadki@gmail.com>
2018-02-04 16:14:44 +0000

    ajout des urls
    ajout des urls

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html>
   <head>
-  <title>Situation</title>
+  <title>openshift red hat cloud platform - php application</title>
   </head>
   <body>
-    <h1>test openshift red hat cloud platform</h1>
+    <h1><a href="/index.php">test openshift red hat cloud platform</a></h1>
+    <h4><a href="/info.php">php info</a></h4>
+    <h4></h4>
+    <h4></h4>
+    <h5></h5>
   </body>
 </html>
