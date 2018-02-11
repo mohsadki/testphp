9668afca23394261cf17390d8cb502fd482cc8fe
mohamed seddik <mohsadki@gmail.com>
2018-02-11 20:34:10 +0000

    echo des variables d'env

--- mysql.php
+++ mysql.php
@@ -1,31 +1,49 @@
 <html>
-  <head>
-  <title>test openshift mysql </title>
-  </head>
-  <body>
-              <h3>test apache2 http server at openshift redhat cloud platform<h3>
-                <?php
-                $servername = "localhost";
-                $username = "msedw";
-                $password = "msedw";
+<head>
+<title>test openshift mysql </title>
+</head>
+<body>
+        <h3>test apache2 http server at openshift redhat cloud platform<h3>
+          <?php
+          $servername = "localhost";
+          $username = "msedw";
+          $password = "msedw";
 
-                // Create connection
-                // $conn = new mysqli($servername, $username, $password);
-                // redhat openshift mysql connection
-                echo "================";
-                echo $MYSQL_USER;
-                echo $MYSQL_PASSWORD;
-                echo "================";
-                //$conn = new mysqli("localhost",$MYSQL_USER,$MYSQL_PASSWORD);
-                // Check connection
-                if ($conn->connect_error) {
-                    echo "Connection failed !";
-                    echo $conn->connect_error;
-                    die("Connection failed: " . $conn->connect_error);
-                }
-                echo "Connected successfully o yee";
+          // Create connection
+          // $conn = new mysqli($servername, $username, $password);
+          // redhat openshift mysql connection
+          echo "================<br>";
+          echo $MYSQL_USER;
+          echo $MYSQL_PASSWORD;
 
-                ?>
+          echo getenv('MYSQL_DATABASE');
+          echo "=<br>";
+          echo getenv('MYSQL_USER');
+          echo "=<br>";
+          echo getenv('MYSQL_PASSWORD');
+          echo "=<br>";
 
-  </body>
+          echo getenv('OPENSHIFT_MYSQL_DB_HOST'); // Host name
+          echo "=<br>";
+          echo getenv('OPENSHIFT_MYSQL_DB_PORT'); // Host port
+          echo "=<br>";
+          echo getenv('OPENSHIFT_MYSQL_DB_USERNAME'); // MySQL username
+          echo "=<br>";
+          echo getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); // MySQL password
+          echo "=<br>";
+          echo getenv('OPENSHIFT_GEAR_NAME'); // Database name
+
+          echo "================<br>";
+          $conn = new mysqli("localhost",$MYSQL_USER,$MYSQL_PASSWORD);
+          // Check connection
+          if ($conn->connect_error) {
+              echo "Connection failed !";
+              echo $conn->connect_error;
+              die("Connection failed: " . $conn->connect_error);
+          }
+          echo "Connected successfully o yee";
+
+          ?>
+
+</body>
 </html>
