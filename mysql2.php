ff32ea2ffb1fdbefe00d42664eb79b774ca86b50
mohamed seddik <mohsadki@gmail.com>
2018-02-17 23:50:20 +0000

    requete sql

--- mysql2.php
+++ mysql2.php
@@ -1,16 +1,4 @@
 <?php
-echo "==connection test===<br>";
-echo $OPENSHIFT_MYSQL_DB_HOST; // Host name
-echo "=<br>";
-echo $OPENSHIFT_MYSQL_DB_PORT; // Host port
-echo "=<br>";
-echo $OPENSHIFT_MYSQL_DB_USERNAME; // MySQL username
-echo "=<br>";
-echo $OPENSHIFT_MYSQL_DB_PASSWORD; // MySQL password
-echo "=<br>";
-echo $OPENSHIFT_GEAR_NAME; // Database name
-echo "=<br>";
-// procedural style
 $mysqli = mysqli_connect(
     getenv('OPENSHIFT_MYSQL_DB_HOST'),
     getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
@@ -26,13 +14,13 @@ if ($mysqli->connect_error) {
 }
 echo "Connected successfully";
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "select id, title, detail, ts from items";
 $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "id: " . $row["id"]. " - title: " . $row["title"]. " - detail: " . $row["detail"].  " -date: " . $row["ts"]. "<br>";
     }
 } else {
     echo "0 results";
