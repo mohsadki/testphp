0af47a27b26c32b4c4b4eca966192beb651248a6
mohamed seddik <mohsadki@gmail.com>
2018-02-17 22:18:06 +0000

    22:18

--- mysql.php
+++ mysql.php
@@ -3,38 +3,9 @@
 <title>test openshift mysql </title>
 </head>
 <body>
-        <h3>test apache2 http server at openshift redhat cloud platform<h3>
+        <h3>MySQL<h3>
           <?php
-          $servername = "172.30.19.227";
-          $username = "msedw";
-          $password = "msedw";
-
-          // Create connection
-          // $conn = new mysqli($servername, $username, $password);
-          // redhat openshift mysql connection
-          echo "=====aaaaaaa=====<br>";
-          echo $MYSQL_USER;
-          echo $MYSQL_PASSWORD;
-
-          echo getenv('MYSQL_DATABASE');
-          echo "=<br>";
-          echo getenv('MYSQL_USER');
-          echo "=<br>";
-          echo getenv('MYSQL_PASSWORD');
-          echo "=<br>";
-
-          echo getenv('OPENSHIFT_MYSQL_DB_HOST'); // Host name
-          echo "=<br>";
-          echo getenv('OPENSHIFT_MYSQL_DB_PORT'); // Host port
-          echo "=<br>";
-          echo getenv('OPENSHIFT_MYSQL_DB_USERNAME'); // MySQL username
-          echo "=<br>";
-          echo getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); // MySQL password
-          echo "=<br>";
-          echo getenv('OPENSHIFT_GEAR_NAME'); // Database name
-
-          echo "================<br>";
-          $conn = new mysqli("172.30.19.227","msedw","msedw");
+          $conn = new mysqli("172.30.24.45","userI4C","172.30.24.45","GSEAAHLQx1mP8nCy","3306");
           // Check connection
           if ($conn->connect_error) {
               echo "Connection failed !";
