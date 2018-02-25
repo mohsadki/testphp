e82ade418969b2947741aa69aa2c8894645357da
mohamed seddik <mohsadki@gmail.com>
2018-02-25 21:36:43 +0000

    0225.2136

--- mysql2.php
+++ mysql2.php
@@ -1,5 +1,5 @@
 <?php
-
+$PHP_EOL = '<br>';
 $conn = mysqli_connect(
     getenv('OPENSHIFT_MYSQL_DB_HOST'),
     getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
@@ -12,19 +12,19 @@ if (!$conn) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+} else {
+    echo "Connected successfully" . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+    $sql = "select * from items";
+  if ($result = mysqli_query($conn, $sql)) {
+      while ($row = mysqli_fetch_row($result)) {
+          printf("%s (%s,%s)\n", $row[0], $row[1], $row[2], $row[3]);
+      }
+      /* free result set */
+      mysqli_free_result($result);
+  }
 }
-echo "Connected successfully";
-echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
 
-$sql = "select * from items";
-
-if ($result = mysqli_query($conn, $sql)) {
-    while ($row = mysqli_fetch_row($result)) {
-        printf("%s (%s,%s)\n", $row[0], $row[1], $row[2], $row[3]);
-    }
-    /* free result set */
-    mysqli_free_result($result);
-}
 
 $conn->close();
 ?>
