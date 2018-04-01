20ee416edaef41a3faeaf7213bbbe9b64683532d
mohamed seddik <mohsadki@gmail.com>
2018-04-01 22:35:11 +0100

    <br>

--- mysql3.php
+++ mysql3.php
@@ -9,16 +9,16 @@ $conn = mysqli_connect(
 );
 
 if (!$conn) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    echo "Error: Unable to connect to MySQL." . $PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . $PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . $PHP_EOL;
 } else {
-    echo "Connected successfully" . PHP_EOL;
-    echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+    echo "Connected successfully" . $PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($conn) . $PHP_EOL;
     $sql = "select * from items";
   if ($result = mysqli_query($conn, $sql)) {
       while ($row = mysqli_fetch_row($result)) {
-          printf("%s (%s,%s)\n", $row[0], $row[1], $row[2], $row[3]);
+          printf("%s (%s,%s,%s)<br>", $row[0], $row[1], $row[2], $row[3]);
       }
       /* free result set */
       mysqli_free_result($result);
